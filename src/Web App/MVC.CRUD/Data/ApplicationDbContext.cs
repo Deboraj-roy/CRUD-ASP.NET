558a0b349364f0a53cad6dac236e6eb2ae91cf19
“DEBORAJ <deborajroy123@gmail.com>
2024-04-25 18:24:28 +0600

    ApplicationDbContext configure

---
+++ src/Web App/MVC.CRUD/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MVC.CRUD.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+    }
+}
