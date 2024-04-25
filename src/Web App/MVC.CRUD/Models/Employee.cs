d49a709725dc9347299f8344b066776a8ef90ee9
“DEBORAJ <deborajroy123@gmail.com>
2024-04-25 18:11:10 +0600

    Model class Added

---
+++ src/Web App/MVC.CRUD/Models/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVC.CRUD.Models
+{
+    public class Employee
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public Double Salary { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Department { get; set; }
+        public DateTime JoinDate { get; set; }
+        public DateTime EntryDate { get; set; }
+    }
+}
