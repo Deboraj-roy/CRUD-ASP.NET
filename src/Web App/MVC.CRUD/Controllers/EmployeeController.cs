ef9e2dae7e6a757a08344cf89bd98640c45f672f
“DEBORAJ <deborajroy123@gmail.com>
2024-04-25 18:48:54 +0600

    Employee Controller Added

---
+++ src/Web App/MVC.CRUD/Controllers/EmployeeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC.CRUD.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
