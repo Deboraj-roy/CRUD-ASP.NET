4e0fdc8a3ea67678b04a65e7a88295cb751aa5f0
“DEBORAJ <deborajroy123@gmail.com>
2024-04-25 19:40:38 +0600

    Section 2 - Display all Employees

--- src/Web App/MVC.CRUD/Controllers/EmployeeController.cs
+++ src/Web App/MVC.CRUD/Controllers/EmployeeController.cs
@@ -1,12 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MVC.CRUD.Data;
+using MVC.CRUD.Models;
 
 namespace MVC.CRUD.Controllers
 {
     public class EmployeeController : Controller
     {
+        private readonly ApplicationDbContext _db;
+        public EmployeeController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
-            return View();
+            //IEnumerable<Employee> objEmployeeList = _db.Employees;
+            //List<Employee> objEmployeeList1 = _db.Employees.ToList();
+            var objEmployeeList = _db.Employees.ToList();
+            return View(objEmployeeList);
         }
     }
 }
