55d845c5956bff4eb9910158260e5ad35d2da5a8
“DEBORAJ <deborajroy123@gmail.com>
2024-07-17 02:45:05 +0600

    PersonController GetAll

--- src/ADO_dot_Net/ADO_dot_Net/Controllers/PersonController.cs
+++ src/ADO_dot_Net/ADO_dot_Net/Controllers/PersonController.cs
@@ -1,61 +1,59 @@
-﻿//using ADO_dot_Net.Data;
-//using Microsoft.AspNetCore.Mvc;
-//using System;
-//using System.Collections.Generic;
-//using System.Net;
-//using System.Net.Http;
-//namespace ADO_dot_Net.Controllers
-//{
-//    [Route("api/Person")]
-//    [ApiController] 
-//    public class PersonController : ControllerBase
-//    {
-//        PersonRepository personRepository;
+﻿using ADO_dot_Net.Data;
+using Microsoft.AspNetCore.Mvc;
+namespace ADO_dot_Net.Controllers
+{
+    [ApiController]
+    [Route("v3/[controller]")]
+    public class PersonController : ControllerBase
+    {
+        PersonRepository personRepository;
 
-//        public PersonController()
-//        { 
-//            personRepository = new PersonRepository();
-//        }
-//        public IActionResult GetPeople()
-//        { 
-//            List<Person> people = personRepository.GetPeople(); // <List<Person>>
-//            return Ok(people);
-//        }
-
-//        public IActionResult GetPersonById(int id)
-//        {
-//            Person person = personRepository.PersonById(id);
-//            if (person == null)
-//            {
-//                return NotFound();
-//            }
-//            return Ok(person);
-//        }
-
-//        public IActionResult AddPerson(Person person)
-//        {
-//            personRepository.CreatePerson(person);
-//            return CreatedAtRoute("DefaultApi", new { id = person.Id }, person); ;
-//        }
-
-//        public IActionResult UpdatePerson(int id, Person person)
-//        {
-//            if (id != person.Id)
-//            {
-//                return BadRequest();
-//            }
-//            var personw = personRepository.PersonById(id);
-//            if (personw is not null)
-//            { 
-//            personRepository.UpdatePerson(person);
-//            return Ok();
-//            }
-//            return NotFound();
-//        }
-//        public IActionResult DeletePerson(int id)
-//        {
-//            personRepository.DeletePerson(id);
-//            return Ok();
-//        }
-//    }
-//}
+        public PersonController()
+        {
+            personRepository = new PersonRepository();
+        }
+        [HttpGet]
+        public IActionResult GetPeople()
+        {
+            var people = personRepository.GetPeople(); // <List<Person>>
+            return Ok(people);
+        }
+        //[HttpGet]
+        //public IActionResult GetPersonById(int id)
+        //{
+        //    Person person = personRepository.PersonById(id);
+        //    if (person == null)
+        //    {
+        //        return NotFound();
+        //    }
+        //    return Ok(person);
+        //}
+        //[HttpPost]
+        //public IActionResult AddPerson(Person person)
+        //{
+        //    personRepository.CreatePerson(person);
+        //    return CreatedAtRoute("DefaultApi", new { id = person.Id }, person); ;
+        //}
+        //[HttpPut]
+        //public IActionResult UpdatePerson(int id, Person person)
+        //{
+        //    if (id != person.Id)
+        //    {
+        //        return BadRequest();
+        //    }
+        //    var personw = personRepository.PersonById(id);
+        //    if (personw is not null)
+        //    {
+        //        personRepository.UpdatePerson(person);
+        //        return Ok();
+        //    }
+        //    return NotFound();
+        //}
+        //[HttpDelete]
+        //public IActionResult DeletePerson(int id)
+        //{
+        //    personRepository.DeletePerson(id);
+        //    return Ok();
+        //}
+    }
+}
