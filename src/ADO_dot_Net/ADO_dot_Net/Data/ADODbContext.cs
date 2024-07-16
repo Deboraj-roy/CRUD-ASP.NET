24ab0bf680c76a212016da39d2a2586aa14094b6
“DEBORAJ <deborajroy123@gmail.com>
2024-07-16 20:49:02 +0600

    ADODbContext With CRUD Operations

---
+++ src/ADO_dot_Net/ADO_dot_Net/Data/ADODbContext.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace ADO_dot_Net.Data
+{
+    public class ADODbContext
+    { 
+        private string ConnectionString { get; set; }
+        public ADODbContext(string connectionString)
+        {
+            ConnectionString = connectionString;
+        }
+
+        public DataTable GetPersons()
+        {
+            DataTable dt = new DataTable();
+            using (SqlConnection connection = new SqlConnection(ConnectionString))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand("SELECT * FROM Person", connection);
+                SqlDataReader reader = command.ExecuteReader();
+                dt.Load(reader);
+            }
+            return dt;
+        }
+        public DataTable GetPersons(int id)
+        {
+            DataTable dt = new DataTable();
+            using (SqlConnection connection = new SqlConnection(ConnectionString))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand($"SELECT * FROM Person WHERE Id = @Id", connection);
+                command.Parameters.AddWithValue("@Id", id);
+                SqlDataReader reader = command.ExecuteReader();
+                dt.Load(reader);
+            }
+            return dt;
+        }
+        public void CreatePerson(Person person)
+        {
+            using (SqlConnection connection = new SqlConnection(ConnectionString))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand("INSERT INTO Person (Name, Address, Email, Phone, Salary) VALUES (@Name, @Address, @Email, @Phone, @Salary)", connection);
+                command.Parameters.AddWithValue("@Name", person.Name);
+                command.Parameters.AddWithValue("@Address", person.Address);
+                command.Parameters.AddWithValue("@Email", person.Email);
+                command.Parameters.AddWithValue("@Phone", person.Phone);
+                command.Parameters.AddWithValue("@Salary", person.Salary);
+                command.ExecuteNonQuery();
+            }
+        }
+
+        public void UpdatePerson(Person person)
+        {
+            using (SqlConnection connection = new SqlConnection(ConnectionString))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand("UPDATE Person SET Name = @Name, Address = @Address, Email = @Email, Phone = @Phone, Salary = @Salary WHERE Id = @Id", connection);
+                command.Parameters.AddWithValue("@Name", person.Name);
+                command.Parameters.AddWithValue("@Address", person.Address);
+                command.Parameters.AddWithValue("@Email", person.Email);
+                command.Parameters.AddWithValue("@Phone", person.Phone);
+                command.Parameters.AddWithValue("@Salary", person.Salary);
+                command.Parameters.AddWithValue("@Id", person.Id);
+                command.ExecuteNonQuery();
+            }
+        }
+
+        public void DeletePerson(int id)
+        {
+            using (SqlConnection connection = new SqlConnection(ConnectionString))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand("DELETE FROM Person WHERE Id = @Id", connection);
+                command.Parameters.AddWithValue("@Id", id);
+                command.ExecuteNonQuery();
+            }
+        }
+    }
+}
--- src/ADO_dot_Net/ADO_dot_Net/Data/Person.cs
+++ src/ADO_dot_Net/ADO_dot_Net/Data/Person.cs
@@ -7,7 +7,7 @@
         public string Address { get; set; }
         public string Email { get; set; } 
         public string Phone { get; set; }
-        public double salary { get; set; }
-        }
+        public double Salary { get; set; }
+        
     }
 }
