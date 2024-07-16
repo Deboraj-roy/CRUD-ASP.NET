8d2026912fe4bc1f0cf54a5c4b1f131a4dad189e
“DEBORAJ <deborajroy123@gmail.com>
2024-07-16 20:22:39 +0600

    new Branch base

--- src/Web App/MVC.CRUD/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace MVC.CRUD.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
