35ea15c05459538d7d4bf9e01301b70a3d53b423
“DEBORAJ <deborajroy123@gmail.com>
2024-07-17 02:40:16 +0600

    Home Controller Works

--- src/ADO_dot_Net/ADO_dot_Net/Controllers/HomeController.cs
+++ src/ADO_dot_Net/ADO_dot_Net/Controllers/HomeController.cs
@@ -1,20 +1,25 @@
-﻿//using ADO_dot_Net.Data;
-//using Microsoft.AspNetCore.Mvc;
+﻿using ADO_dot_Net.Controllers;
+using ADO_dot_Net.Data;
+using Microsoft.AspNetCore.Mvc;
 
-//[ApiController]
-//[Route("v3/[controller]")]
-//public class HomeController : ControllerBase
-//{
-//    private readonly PersonRepository _personRepository;
+[ApiController]
+[Route("v3/[controller]")]
+public class HomeController : ControllerBase
+{
+     PersonRepository _personRepository;
 
-//    public HomeController(PersonRepository personRepository)
-//    {
-//        _personRepository = personRepository;
-//    }
+    public HomeController()
+    {
+        _personRepository = new PersonRepository();
+    }
 
-//    public IActionResult Index()
-//    {
-//        var persons = _personRepository.GetPeople();
-//        return Ok(persons);
-//    }
-//}
+    //private readonly ILogger<HomeController> _logger;
+    //public HomeController(ILogger<HomeController> logger) => _logger = logger;
+    [HttpGet]
+
+    public IActionResult Get()
+    {
+        var persons = _personRepository.GetPeople();
+        return Ok(persons);
+    }
+}
