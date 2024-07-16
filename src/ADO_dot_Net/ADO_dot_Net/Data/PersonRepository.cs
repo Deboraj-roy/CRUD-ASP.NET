9c7f1aa515a85b445ca637f0e79a9011d3d59c8a
“DEBORAJ <deborajroy123@gmail.com>
2024-07-16 21:11:28 +0600

    Create a business logic layer

---
+++ src/ADO_dot_Net/ADO_dot_Net/Data/PersonRepository.cs
@@ -0,0 +1,78 @@
+﻿using System.Data;
+
+namespace ADO_dot_Net.Data
+{
+    public class PersonRepository
+    {
+        private string ConnectionString { get; set; }
+        private ADODbContext Context { get; set; }
+        public PersonRepository(string connectionString)
+        {
+            ConnectionString = connectionString;
+            Context = new ADODbContext(ConnectionString);
+        }
+        public List<Person> GetPeople()
+        {
+            DataTable dt = Context.GetPersons();
+            List<Person> people = new List<Person>();
+            foreach (DataRow row in dt.Rows)
+            {
+                people.Add(new Person
+                {
+                    Id = Convert.ToInt32(row["Id"]),
+                    Name = row["Name"].ToString(),
+                    Address = row["Address"].ToString(),
+                    Email = row["Email"].ToString(),
+                    Phone = row["Phone"].ToString(),
+                    Salary = Convert.ToDouble(row["Salary"])
+                });
+            }
+            return people;
+        }
+
+        public Person PersonById(int id)
+        {
+            if (id == 0 || id < 0)
+            {
+                return null;
+            }
+
+            DataTable dt = Context.GetPersons(id);
+            if (dt.Rows.Count > 0)
+            {
+
+                Person person = new Person
+                {
+                    Id = Convert.ToInt32(dt.Rows[0]["Id"]),
+                    Name = dt.Rows[0]["Name"].ToString(),
+                    Address = dt.Rows[0]["Address"].ToString(),
+                    Email = dt.Rows[0]["Email"].ToString(),
+                    Phone = dt.Rows[0]["Phone"].ToString(),
+                    Salary = Convert.ToDouble(dt.Rows[0]["Salary"])
+                };
+                return person;
+
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public void CreatePerson(Person person)
+        {
+            Context.CreatePerson(person);
+        }
+
+        public void UpdatePerson(Person person)
+        {
+            Context.UpdatePerson(person);
+        }
+        public void DeletePerson(int id)
+        {
+            Context.DeletePerson(id);
+        }
+    }
+
+}
+
