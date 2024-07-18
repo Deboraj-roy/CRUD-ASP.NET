95206ec1ea421ab6684044574bce05d59a29e427
“DEBORAJ <deborajroy123@gmail.com>
2024-07-18 15:25:22 +0600

    CRUD Consol

---
+++ src/ADO_dot_Net/Rimon_NATH_conDemo/ADODBContext.cs
@@ -0,0 +1,134 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rimon_NATH_conDemo
+{
+    public class ADODBContext
+    {
+        public string ConnectionString { get; set; }
+
+        public ADODBContext(string connectionString)
+        {
+            ConnectionString = connectionString;
+        }
+
+
+        //view all students
+        public List<Student> GetStudents()
+        {
+            List<Student> students = new List<Student>();
+            using (SqlConnection connection = new SqlConnection(ConnectionString))
+            {
+                connection.Open();
+                SqlCommand sqlCommand = new SqlCommand("SELECT * From Students", connection);
+
+                SqlDataReader reader = sqlCommand.ExecuteReader();
+                while (reader.Read())
+                {
+                    Student student = new Student
+                    {
+                        Id = reader.GetInt32(0),
+                        Name = reader.GetString(1),
+                        Age = reader.GetInt32(2)
+                    };
+                    students.Add(student);
+                }
+            }
+            return students;
+        }
+
+        //view single student
+        public Student GetStudent(int id)
+        {
+            Student student = new Student();
+            using (SqlConnection connection = new SqlConnection(ConnectionString))
+            {
+                connection.Open();
+                SqlCommand sqlCommand = new SqlCommand("SELECT * From Students WHERE Id = @id", connection);
+                sqlCommand.Parameters.AddWithValue("@id", id);
+                SqlDataReader reader = sqlCommand.ExecuteReader();
+                while (reader.Read())
+                {
+                    student.Id = reader.GetInt32(0);
+                    student.Name = reader.GetString(1);
+                    student.Age = reader.GetInt32(2);
+                }
+            }
+            return student;
+        }
+
+        //add student
+        public bool AddStudent(Student student)
+        {
+            if (student != null)
+            {
+
+                using (SqlConnection connection = new SqlConnection(ConnectionString))
+                {
+                    connection.Open();
+                    SqlCommand sqlCommand = new SqlCommand("INSERT INTO Students (Id, Name, Age) VALUES(@Id, @name, @age)", connection);
+                    sqlCommand.Parameters.AddWithValue("@Id", student.Id);
+                    sqlCommand.Parameters.AddWithValue("@name", student.Name);
+                    sqlCommand.Parameters.AddWithValue("@age", student.Age);
+                    sqlCommand.ExecuteNonQuery();
+                }
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        //delete student
+        public bool DeleteStudent(int id)
+        {
+            if (id > 0)
+            {
+                using (SqlConnection connection = new SqlConnection(ConnectionString))
+                {
+                    connection.Open();
+                    SqlCommand sqlCommand = new SqlCommand("DELETE FROM Students WHERE Id = @id", connection);
+                    sqlCommand.Parameters.AddWithValue("@id", id);
+                    sqlCommand.ExecuteNonQuery();
+                }
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        //update student
+        public bool UpdateStudent(Student student)
+        {
+            if (student != null)
+            {
+
+                using (SqlConnection connection = new SqlConnection(ConnectionString))
+                {
+                    connection.Open();
+                    SqlCommand sqlCommand = new SqlCommand("UPDATE Students SET Name = @name, Age = @age WHERE Id = @id", connection);
+                    sqlCommand.Parameters.AddWithValue("@id", student.Id);
+                    sqlCommand.Parameters.AddWithValue("@name", student.Name);
+                    sqlCommand.Parameters.AddWithValue("@age", student.Age);
+                    sqlCommand.ExecuteNonQuery();
+
+                    return true;
+                }
+
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+    }
+}
--- src/ADO_dot_Net/Rimon_NATH_conDemo/Program.cs
+++ src/ADO_dot_Net/Rimon_NATH_conDemo/Program.cs
@@ -1,2 +1,34 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Rimon_NATH_conDemo;
+
 Console.WriteLine("Hello, World!");
+string connectionString = "Server =.\\SQLEXPRESS; Database = ADO_DOT_NET; User Id = aspnetb9; Password = 123456; Trust Server Certificate=True;";
+ADODBContext context = new ADODBContext(connectionString);
+//var students = context.GetStudents();
+
+//foreach (var item in students)
+//{
+//    Console.WriteLine($"ID: {item.Id} Name: {item.Name} Age: {item.Age}");
+//}
+//int id = int.Parse(Console.ReadLine());
+//var student = context.GetStudent(id);
+//Console.WriteLine($"ID: {student.Id} Name: {student.Name} Age: {student.Age}");
+
+//Console.WriteLine("Give student data to add Student");
+Console.Write("ID: ");
+int id = int.Parse(Console.ReadLine());
+//Console.Write("Name: ");
+//string name = Console.ReadLine();
+//Console.Write("Age: ");
+//int age = int.Parse(Console.ReadLine());
+//Student newstudent = new Student
+//{
+//    Id = id,
+//    Name = name,
+//    Age = age
+//};
+//context.AddStudent(newstudent);
+
+var result = context.DeleteStudent(id);
+Console.WriteLine(result);
+ 
